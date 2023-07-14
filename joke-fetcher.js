ea150ca85ae70e9884f4001dda3aaf23322c0348
jwhaynes <jhaynes2112@gmail.com>
2023-07-13 22:02:12 -0400

    added function for checkbox toggle

--- joke-fetcher.js
+++ joke-fetcher.js
@@ -14,7 +14,7 @@ const newJoke = () => {
         typeDispaly.innerHTML = res['type']
         setupDisplay.innerHTML = res['setup']
         punchlineDispaly.innerHTML = res['punchline']
-        
+
         console.log(res)})
     .catch(error=>console.log(error))
 };
@@ -23,3 +23,23 @@ const newJoke = () => {
 const button = document.getElementById("new-joke-btn");
 
 button.onclick = newJoke;
+
+
+const updatePunchlineVisibility = () => {
+    const checkbox = document.getElementById("punchline-checkbox");
+    const punchlineText = document.getElementById("joke-punchline");
+
+    if (checkbox.checked) {
+        punchlineText.style.display = "block";
+        console.log("block mode")
+    } else {
+        punchlineText.style.display = "none";
+        console.log("none mode")
+    }
+    console.log(punchlineText.style.display)
+}
+
+const checkbox = document.getElementById("punchline-checkbox");
+
+checkbox.onclick = updatePunchlineVisibility;
+
